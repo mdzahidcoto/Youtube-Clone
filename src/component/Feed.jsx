c69c78c5a49a94a1d1297a31a63233883ed46788
Md. Zahid Sarker <84657860+mdzahidcoto@users.noreply.github.com>
2022-09-26 16:24:14 +0600

    navbar ui is done

---
+++ src/component/Feed.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import {  } from '@mui/icons-material';
+
+const Feed = () => {
+  return (
+    <div>Feed</div>
+  )
+}
+
+export default Feed
